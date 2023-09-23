06366d84fdb6332d62565b54ace3bd9a3d094bd0
OydinJurayeva <oydinjorayeva1020@gmail.com>
2023-09-23 12:56:20 +0500

    new

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,23 +1,28 @@
-import Filter from "../components/Filters"
-import { ProductsContainer, PaginationsContainer } from "../components"
-import { customFetch } from "../utils"
+import Filter from "../components/Filters";
+import { ProductsContainer, PaginationsContainer } from "../components";
+import { customFetch } from "../utils";
 
-const url = "/products"
+const url = "/products";
 export const loader = async ({ request }) => {
-   const response = await customFetch(url)
-   const products = response.data.data
-   const meta = response.data.meta
-   return { products, meta }
-}
+  const params = Object.fromEntries([
+    ...new URL(request.url).searchParams.entries(),
+  ]);
+  const response = await customFetch(url, {
+    params,
+  });
+  const products = response.data.data;
+  const meta = response.data.meta;
+  return { products, params, meta };
+};
 
 function Products() {
-   return (
-      <div>
-         <Filter />
-         <ProductsContainer />
-         <PaginationsContainer />
-      </div>
-   )
+  return (
+    <div className="py-20">
+      <Filter />
+      <ProductsContainer />
+      <PaginationsContainer />
+    </div>
+  );
 }
 
-export default Products
+export default Products;
